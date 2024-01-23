b7cee1aaff2734f90b1308ad93d30c6eed1516ae
Namba1337 <ya.nos96@gmail.com>
2024-01-23 20:30:07 +0500

    fix: updating the directory when deleting files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,17 +5,26 @@ async function copyDir(src, dest) {
     // Создаем папку назначения, если она еще не существует
     await fs.mkdir(dest, { recursive: true });
 
-    // Читаем содержимое исходной папки
-    const entries = await fs.readdir(src, { withFileTypes: true });
+    // Читаем содержимое исходной и целевой папок
+    const srcEntries = await fs.readdir(src, { withFileTypes: true });
+    const destEntries = await fs.readdir(dest, { withFileTypes: true });
 
-    for (let entry of entries) {
-        const srcPath = path.join(src, entry.name);
-        const destPath = path.join(dest, entry.name);
+    // Удаляем файлы из целевой папки, которых нет в исходной папке
+    for (let destEntry of destEntries) {
+        if (!srcEntries.find(srcEntry => srcEntry.name === destEntry.name)) {
+            await fs.rm(path.join(dest, destEntry.name), { recursive: true, force: true });
+        }
+    }
 
-        if (entry.isDirectory()) {
+    // Копируем файлы из исходной папки в целевую
+    for (let srcEntry of srcEntries) {
+        const srcPath = path.join(src, srcEntry.name);
+        const destPath = path.join(dest, srcEntry.name);
+
+        if (srcEntry.isDirectory()) {
             // Если элемент является папкой, рекурсивно копируем его содержимое
             await copyDir(srcPath, destPath);
-        } else if (entry.isFile()) {
+        } else if (srcEntry.isFile()) {
             // Если элемент является файлом, копируем его
             await fs.copyFile(srcPath, destPath);
         }
